7c2b856a20be27b90a01eb43035c37140147e48f
Eleazar Alfredo, ALFRIADEZ YRIARTE <ealfriadez@gmail.com>
2024-07-18 00:19:52 -0500

    Avanzando con las pruebas unitarias 37.15

--- src/test/java/com/ntloc/test_junit/customer/CustomerRepositoryTest.java
+++ src/test/java/com/ntloc/test_junit/customer/CustomerRepositoryTest.java
@@ -4,29 +4,65 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.Optional;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureDataJpa;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
 
-@SpringBootTest
+@DataJpaTest
+@Testcontainers
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class CustomerRepositoryTest {
 
+	@Container
+	@ServiceConnection
+	static PostgreSQLContainer<?> postgreSQLContainer = new PostgreSQLContainer<>(DockerImageName.parse("postgres:16.2"));
 	@Autowired
 	CustomerRepository underTest;
-	
+
+	@BeforeEach
+	void setUp() {
+		String email = "salfriadez@gmail.com";
+		Customer customer = Customer.create(
+				"Sebastian Alfredo",
+				email,
+				"San Francisco Mz E Lt 2");
+		underTest.save(customer);
+	}
+
+	@AfterEach
+	void tearDown() {
+		underTest.deleteAll();
+	}
+
+	@Test
+	void canEstablishedConnection(){
+		assertThat(postgreSQLContainer.isCreated()).isTrue();
+		assertThat(postgreSQLContainer.isRunning()).isTrue();
+	}
+
 	@Test
 	void shouldReturnCustomerWhenFindByEmail() {
 		//given
-		String email = "salfriadez@gmail.com";
-		Customer customer = Customer.create(
-				"Sebastian Alfredo", 
-				email, 
-				"San Francisco Mz E Lt 2"); 
-		underTest.save(customer);
 		//when
-		Optional<Customer> customerByEmail = underTest.findByEmail(email);
+		Optional<Customer> customerByEmail = underTest.findByEmail("salfriadez@gmail.com");
 		//then
 		assertThat(customerByEmail).isPresent();
 	}
 
+	@Test
+	void shouldNotCustomerWhenFindByEmailIsNoPresent() {
+		//given
+		//when
+		Optional<Customer> customerByEmail = underTest.findByEmail("ealfriadez@gmail.com");
+		//then
+		assertThat(customerByEmail).isNotPresent();
+	}
 }
